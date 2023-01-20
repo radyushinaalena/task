2d1d675f029b206010b0a8f8f4d48c72f6e9aaf0
Alena <al.shel@bk.ru>
2023-01-20 20:35:38 +0300

    Деление строки на две части

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        divideLine("Hello_world!");
     }
+
+    private static void divideLine(String line) {
+        int symbols = line.length();
+        if (symbols % 2 != 0) {
+            System.out.println("Не делится пополам");
+        } else if (line.isEmpty()) {
+            System.out.println("Строка пустая");
+        } else {
+            int symbolsNew = symbols / 2;
+            String firstHalf = line.substring(0, symbolsNew);
+            String secondHalf = line.substring(symbolsNew);
+            System.out.println(firstHalf + " " + secondHalf);
+        }
+    }
+
 }
